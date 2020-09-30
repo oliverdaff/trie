4be33e2bad084ec01c9f27f56338af7d2350eab1
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-09-30 15:18:33 +1000

    check expected size

--- tire_test.go
+++ tire_test.go
@@ -10,22 +10,26 @@ func TestNewTrieNode(t *testing.T) {
 		key           string
 		value         interface{}
 		keyIndex      int
+		expectedSize  int
 		errorExpected bool
 	}{
-		{"www.testing.com", nil, 0, false},
-		{"www.testing.com", nil, len("www.testing.com"), false},
-		{"www.testing.com", nil, 100, true},
-		{"www.testing.com", nil, -1, true},
+		{"www.testing.com", nil, 0, 1, false},
+		{"www.testing.com", nil, len("www.testing.com"), 0, false},
+		{"www.testing.com", nil, 100, 0, true},
+		{"www.testing.com", nil, -1, 0, true},
 	}
 	for _, tt := range tests {
 		testname := fmt.Sprintf("%s", tt.key)
 		t.Run(testname, func(t *testing.T) {
-			_, err := newTrieNode(tt.key, tt.value, tt.keyIndex)
+			node, err := newTrieNode(tt.key, tt.value, tt.keyIndex)
 			if !tt.errorExpected && err != nil {
 				t.Errorf("Error was not expected")
 			}
 			if tt.errorExpected && err == nil {
 				t.Errorf("Error was not expected")
+			}
+			if !tt.errorExpected && node.size != tt.expectedSize {
+				t.Errorf("Expected size %d go %d", tt.expectedSize, node.size)
 			}
 		})
 	}
