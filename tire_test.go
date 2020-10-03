616884acd87926247cc9a3a98fae0c431378c045
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-10-03 21:10:07 +1000

    added contains test

--- tire_test.go
+++ tire_test.go
@@ -64,7 +64,7 @@ func TestPut(t *testing.T) {
 	}
 }
 
-func GetNode(t *testing.T) {
+func TestGetNode(t *testing.T) {
 	var tests = []struct {
 		params    []PutValue
 		query     string
@@ -96,6 +96,40 @@ func GetNode(t *testing.T) {
 			}
 			if !tt.nodeFound && queryResult != nil {
 				t.Errorf("Expected null node for query %s got %v", tt.query, queryResult)
+			}
+		})
+	}
+}
+
+func TestContains(t *testing.T) {
+	var tests = []struct {
+		params   []PutValue
+		query    string
+		keyFound bool
+	}{
+		{[]PutValue{ // node found
+			PutValue{"www.test.com", 1, 1, true},
+		}, "www.test.com", true},
+		{[]PutValue{ // node not found
+			PutValue{"www.example.com", 1, 1, true},
+		}, "www.test.com", false},
+	}
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%v", tt.params)
+		t.Run(testname, func(t *testing.T) {
+			node := newTrieNode("", nil)
+			for _, param := range tt.params {
+				newKey := node.put(param.key, param.value)
+				if node.size != param.expectedSize {
+					t.Errorf("Expected size %d go %d", param.expectedSize, node.size)
+				}
+				if newKey != param.expectedNewKey {
+					t.Errorf("Expected newKey %t go %t", param.expectedNewKey, newKey)
+				}
+			}
+			keyFound := node.contains(tt.query)
+			if tt.keyFound != keyFound {
+				t.Errorf("Expected key found %t got %t", tt.keyFound, keyFound)
 			}
 		})
 	}
